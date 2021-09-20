25b4d63e5bd15daa902439f1b2510fc4f21d9ad6
Liamgoh <64313674+Liamgoh@users.noreply.github.com>
2021-09-20 14:56:33 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import pi, { doublePi, triplePi } from "./math.js";
+
+ReactDOM.render(
+  <ul>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
+
+//doublePi and triplePi are functions so need ()
